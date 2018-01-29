d63f82ccee0c5bd62c2ccc7de5da17b28c877f00
Michael Jones <jonesmich@armadapower.com>
2018-01-29 18:21:21 -0500

    Adding java class for Week 2 ex.

--- src/com/michaeljones/week_1/Main.java
+++ src/com/michaeljones/week_1/Main.java
@@ -4,16 +4,24 @@ package com.michaeljones.week_1;
 public class Main {
 
     public static void main(String[] args) {
-        int numberOfAnimals = 3;
-        String[] animals = new String[numberOfAnimals];
+        String city = "Columbus";
+        String zipCode = "43215";
 
-        animals[0] = "cat";
-        animals[1] = "dog";
-        animals[2] = "chicken";
+        int majVersion = 1;
+        int minorVersion = 2;
+        int release = 3;
 
-        System.out.println(animals[1]);
+        System.out.println(majVersion + "." + minorVersion + "." + release);
+
+
+        int highTemperatures[] = {32, 25, 27, 40, 45};
+        int highTemperatureCt = 5;
+
+        float averageHigh = (highTemperatures[0] + highTemperatures[1] + highTemperatures[2] + highTemperatures[3] +
+                highTemperatures[4]) / (float) highTemperatureCt;
+
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+        System.out.println("Average High Temperature: " + averageHigh);
     }
 }
-
-
-// Here
---
+++ src/com/michaeljones/week_2/Main.java
@@ -0,0 +1,7 @@
+package com.michaeljones.week_2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Week 2");
+    }
+}
